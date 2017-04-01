9910cb019f553762844f0d75a6b33ed205588f41
Marcus Soll <Superschlumpf@web.de>
2017-04-01 03:42:18 +0200

    Formatting

--- src/languageinterface.h
+++ src/languageinterface.h
@@ -1,18 +1,18 @@
 /*
  * Copyright 2017 Marcus Soll
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 #ifndef LANGUAGEINTERFACE_H
@@ -27,7 +27,7 @@ class LanguageInterface : public QObject
     Q_OBJECT
 
 public:
-    LanguageInterface(QObject* parent = 0);
+    LanguageInterface(QObject *parent = 0);
 
 public slots:
     QVariantList getAllLanguages();
@@ -37,7 +37,7 @@ public slots:
     bool renameLanguage(int id, QString name);
     QVariantList getVocabularyByLanguage(int id);
     int countVocabularyWithLanguage(int id);
-    
+
 private:
 };
 
